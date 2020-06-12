b37e902e678c277f01c673b5d76f8699638cab43
liviaalmeida <leave.ah@gmail.com>
2020-06-11 21:13:39 -0300

    Added i18n

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = {
         prependData: '@import "~@/assets/scss/variables.scss"; @import "~@/assets/scss/fonts.scss";'
       }
     }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'pt',
+      fallbackLocale: 'en',
+      localeDir: 'assets/locales',
+      enableInSFC: false
+    }
   }
 }
