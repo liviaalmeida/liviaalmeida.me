ccd186cf67a8c217b5b5d46e19c8a80f161e294b
Lívia Almeida <leave.ah@gmail.com>
2021-01-01 23:14:24 -0300

    Importing inviewport

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,15 @@ import AnimatedTitle from '@/ds/AnimatedTitle.vue'
 import ProgressBar from '@/ds/ProgressBar.vue'
 import Timeline from '@/ds/Timeline.vue'
 
+import InViewport from 'vue-in-viewport-directive'
+
 Vue.component('AnimatedIcon', AnimatedIcon)
 Vue.component('AnimatedLink', AnimatedLink)
 Vue.component('AnimatedTitle', AnimatedTitle)
 Vue.component('ProgressBar', ProgressBar)
 Vue.component('Timeline', Timeline)
+
+Vue.directive('view', InViewport)
 
 Vue.config.productionTip = false
 
